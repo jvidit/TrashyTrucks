b8c464dbc2a8395c69a1e6e66328c2a9b652a955
Ashish R Nair <46749011+ashish180199@users.noreply.github.com>
2019-04-20 18:53:16 +0530

    Basic

---
+++ Trashy Trucks/Assets/Scripts/GameAssets.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    public static GameAssets instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public Sprite snakeHeadSprite;
+}
